104050d681c021dffe41add2f0d84d0acb06d791
Erick Navarro <erick@navarro.io>
2025-05-03 12:36:58 -0600

    Add new style to database run page

--- webapp/assets/js/app.js
+++ webapp/assets/js/app.js
@@ -126,3 +126,28 @@ liveSocket.connect();
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket;
+
+const themeToggle = document.getElementById("theme-toggle");
+
+// Check for saved theme preference or use system preference
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+// Toggle theme
+themeToggle.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+
+  // Save preference
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.theme = "dark";
+  } else {
+    localStorage.theme = "light";
+  }
+});
