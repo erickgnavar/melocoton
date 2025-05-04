5fb8ef840113399382d517c40999ccee31a3e0ef
Erick Navarro <erick@navarro.io>
2025-05-03 18:05:01 -0600

    Redirect home to databases index page
    Also remove home content because this page won't be rendered anymore

--- webapp/lib/melocoton_web/controllers/page_controller.ex
+++ webapp/lib/melocoton_web/controllers/page_controller.ex
@@ -2,9 +2,7 @@ defmodule MelocotonWeb.PageController do
   use MelocotonWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    redirect(conn, to: ~p"/databases")
   end
 
   def export_csv(conn, %{"pid" => pid_str}) do
