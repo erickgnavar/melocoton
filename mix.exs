9f07591028896b85002472f42d5de6789257e147
Erick Navarro <erick@navarro.io>
2025-06-24 18:44:58 -0600

    Fix elixir version in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Melocoton.MixProject do
     [
       app: :melocoton,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       test_coverage: [summary: [threshold: 40]],
