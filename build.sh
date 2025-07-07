ac5317b55727187bb7b8afcda5a4f3f3d7dca439
Erick Navarro <erick@navarro.io>
2025-07-07 08:33:32 -0600

    feat: add support for linux releases

--- build.sh
+++ build.sh
@@ -15,10 +15,30 @@ npm install --prefix assets
 # compile assets
 MIX_ENV=prod mix assets.deploy
 
-# compile sidebar elixir application
-BURRITO_TARGET=macos MIX_ENV=prod mix release || exit 1
+# set up compilation target
+OS_NAME=$(uname -o)
+echo "Running on $OS_NAME..."
 
-cp ./burrito_out/melocoton_macos ./src-tauri/binaries/webserver
+if [[ "$OS_NAME" == "Darwin" ]]; then
+  export BURRITO_TARGET=macos
+elif [[ "$OS_NAME" == "GNU/Linux" ]]; then
+  export BURRITO_TARGET=linux
+else
+  echo "Unsupported platform"
+  exit 1
+fi
+
+# compile sidebar elixir application
+MIX_ENV=prod mix release || exit 1
+
+if [[ "$OS_NAME" == "Darwin" ]]; then
+  cp ./burrito_out/melocoton_macos ./src-tauri/binaries/webserver
+elif [[ "$OS_NAME" == "GNU/Linux" ]]; then
+  cp ./burrito_out/melocoton_linux ./src-tauri/binaries/webserver
+else
+  echo "Unsupported platform"
+  exit 1
+fi
 
 cd src-tauri || exit 1
 
