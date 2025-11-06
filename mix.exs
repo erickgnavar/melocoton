062e83d3ea412b300996698628148f0a006ef0ac
Erick Navarro <erick@navarro.io>
2025-11-06 10:38:36 -0600

    chore: upgrade zig to 0.15.2

--- mix.exs
+++ mix.exs
@@ -74,7 +74,7 @@ defmodule Melocoton.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:burrito, "~> 1.4"},
+      {:burrito, "~> 1.5"},
       {:csv, "~> 3.2"}
     ]
   end
