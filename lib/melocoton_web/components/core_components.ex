f09dac8d73e208747bddb50925cf00a2930f41d4
Erick Navarro <erick@navarro.io>
2026-03-19 23:54:02 -0600

    chore: improve styles of flash messages

--- lib/melocoton_web/components/core_components.ex
+++ lib/melocoton_web/components/core_components.ex
@@ -114,8 +114,10 @@ defmodule MelocotonWeb.CoreComponents do
       role="alert"
       class={[
         "fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 ring-1",
-        @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
-        @kind == :error && "bg-rose-50 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
+        @kind == :info &&
+          "bg-emerald-50 dark:bg-emerald-900/30 text-emerald-800 dark:text-emerald-200 ring-emerald-500 dark:ring-emerald-600",
+        @kind == :error &&
+          "bg-rose-50 dark:bg-rose-900/30 text-rose-900 dark:text-rose-200 ring-rose-500 dark:ring-rose-600"
       ]}
       {@rest}
     >
@@ -125,8 +127,12 @@ defmodule MelocotonWeb.CoreComponents do
         {@title}
       </p>
       <p class="mt-2 text-sm leading-5">{msg}</p>
-      <button type="button" class="group absolute top-1 right-1 p-2" aria-label={gettext("close")}>
-        <.icon name="hero-x-mark-solid" class="h-5 w-5 opacity-40 group-hover:opacity-70" />
+      <button
+        type="button"
+        class="group absolute top-1 right-1 p-2 opacity-50 hover:opacity-100"
+        aria-label={gettext("close")}
+      >
+        <.icon name="hero-x-mark-solid" class="h-5 w-5" />
       </button>
     </div>
     """
