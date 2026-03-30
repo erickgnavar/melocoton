60ebd789b96384afbee350eb8e8037f0cb88a6d0
Erick Navarro <erick@navarro.io>
2026-03-29 19:12:57 -0600

    chore: upgrade tailwind to latest patch version on 3.4.X branch

--- config/config.exs
+++ config/config.exs
@@ -34,7 +34,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.4.3",
+  version: "3.4.17",
   melocoton: [
     args: ~w(
       --config=tailwind.config.js
